17174c6ccf8c034b053ad01a9716e7a2d9ecae8e
kimminjoo <kmjoo0259@naver.com>
2023-02-14 21:42:23 +0900

    02230214 home

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,15 @@
 <template>
     <div>
-        <p>{{ msg }}</p>
-        <p v-text="msg"></p>
-        <p v-html="htmlStr"></p>
-        <p v-text="htmlStr"></p>
+
     </div>
 </template>
 
 <script>
-import { ref } from '@vue/reactivity'
 export default {
     setup () {
-        const msg = ref('안녕하세요');
-        const htmlStr = ref('<strong>안녕!!</strong>');
+        
 
-        return {
-            msg,
-            htmlStr
-        }
+        return {}
     }
 }
 </script>
